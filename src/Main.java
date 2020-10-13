f4395383060b32f663bdca933c648582a94756ac
Marat <imd_90@mail.ru>
2020-10-13 18:58:03 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+gitpublic class Main {
+    public static void main(String[] args) {
+        int sum = 1000;
+        int percent = 3;
+        int income = sum * percent / 100;
+        System.out.println(income);
+    }
+}
