abea47b50945aced8a7f5e9048618630a5ba2015
Марат Идиатуллин <44940845+IMaratD@users.noreply.github.com>
2020-10-13 19:45:42 +0300

    Added calculation of income

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-gitpublic class Main {
+public class Main {
     public static void main(String[] args) {
         int sum = 1000;
         int percent = 3;
-        int income = sum * percent / 100;
+        int monthsNum = 12;
+        int income = sum * percent / 100 * monthsNum;
         System.out.println(income);
     }
 }
